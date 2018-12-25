6c202d0cd751db99ba0aa80336663b9fff30bf64
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2018-12-25 12:09:55 +0000

    beautify

--- admin/includes/page.php
+++ admin/includes/page.php
@@ -1,29 +1,29 @@
 <div id="page-wrapper">
 
-<div class="container-fluid">
+    <div class="container-fluid">
 
-    <!-- Page Heading -->
-    <div class="row">
-        <div class="col-lg-12">
-            <h1 class="page-header">
-                Welcome to Admin
-                <small>Dashboard</small>
-            </h1>
-            <ol class="breadcrumb">
-                <li>
-                    <i class="fa fa-dashboard"></i>  <a href="index.php">Dashboard</a>
-                </li>
-                <li class="active">
-                    <i class="fa fa-file"></i> dasboard
-                </li>
-            </ol>
-            
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Welcome to Admin
+                    <small>Dashboard</small>
+                </h1>
+                <ol class="breadcrumb">
+                    <li>
+                        <i class="fa fa-dashboard"></i> <a href="index.php">Dashboard</a>
+                    </li>
+                    <li class="active">
+                        <i class="fa fa-file"></i> dasboard
+                    </li>
+                </ol>
+
+            </div>
         </div>
-    </div>
-    <!-- /.row -->
+        <!-- /.row -->
 
-</div>
-<!-- /.container-fluid -->
+    </div>
+    <!-- /.container-fluid -->
 
 </div>
 <!-- /#page-wrapper -->
--- search.php
+++ search.php
@@ -1,9 +1,10 @@
 <?php include "includes/header.php";?>
 <?php include "includes/db.php";?>
+
 <body>
 
-   
-   <?php include "includes/navigation.php";?>
+
+    <?php include "includes/navigation.php";?>
     <!-- Page Content -->
     <div class="container">
 
@@ -11,7 +12,7 @@
 
             <!-- Blog Entries Column -->
             <div class="col-md-8">
-            <?php
+                <?php
                 
                 if(isset($_POST['submit'])){
                     $search =$_POST['search'];
@@ -43,16 +44,22 @@
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#"><?php echo $post_title?></a>
+                    <a href="#">
+                        <?php echo $post_title?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php"><?php echo $post_author?></a>
+                    by <a href="index.php">
+                        <?php echo $post_author?></a>
                 </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date?></p>
+                <p><span class="glyphicon glyphicon-time"></span> Posted on
+                    <?php echo $post_date?>
+                </p>
                 <hr>
-                <img class="img-responsive" src=<?php echo "image/".$post_image?> alt="">
+                <img class="img-responsive" src=<?php echo "image/" .$post_image?> alt="">
                 <hr>
-                <p><?php echo $post_content?></p>
+                <p>
+                    <?php echo $post_content?>
+                </p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
@@ -69,16 +76,16 @@
                     </li>
                 </ul>
 
-                </div>   
+            </div>
             <!-- Blog Sidebar Widgets Column -->
-            
-         <?php include "includes/sidebar.php";?> 
+
+            <?php include "includes/sidebar.php";?>
         </div>
         <!-- /.row -->
 
         <hr>
 
-        <?php include "includes/footer.php";?>      
+        <?php include "includes/footer.php";?>
     </div>
     <!-- /.container -->
 
